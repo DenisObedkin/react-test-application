d9908ffc39c63913a756c84579848c712464b8d5
deobed <deobed7@gmail.com>
2021-07-18 11:24:04 +0300

    Add router and hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from 'react-router-dom';
 
-function App() {
+const List = lazy(() => import('./components/list'));
+const ToDoForm = lazy(() => import('./components/forms/todoform'));
+
+export default function App() {
+  const elements = [
+    'First sample message',
+    'Second sample message',
+    'Third sample message',
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div>Content loading...</div>}>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+          </nav>
+          <hr />
+          <List elements={elements} />
+          <br />
+          <Link to="/create">Create</Link>
+
+          <Switch>
+            <Route exact path="/"></Route>
+            <Route path="/create" component={ToDoForm} />
+          </Switch>
+        </div>
+      </Router>
+    </Suspense>
   );
 }
-
-export default App;
---
+++ src/components/forms/todoform/toDoForm.js
@@ -0,0 +1,39 @@
+import React, { Suspense, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function ToDoForm(props) {
+  const { element } = props;
+  const [value, setValue] = useState(element ?? '');
+  let title = '';
+  if (element) title = `Update ${value.toLowerCase()}`;
+  else title = `Create ${value === '' ? 'new element' : value}`;
+
+  useEffect(() => {
+    setValue(element ?? '');
+  }, [element]);
+
+  let history = useHistory();
+
+  return (
+    <div>
+      <Suspense fallback={<div>Content loading...</div>}>
+        <h4>{title}</h4>
+        <input
+          type="text"
+          placeholder="Input some text..."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        &nbsp;
+        <input type="button" value="Save" />
+        &nbsp;
+        <input
+          type="button"
+          value="Cancel"
+          onClick={(e) => history.push('/')}
+        />
+        &nbsp;
+      </Suspense>
+    </div>
+  );
+}
